08185d609a234d5df379040a62b8699d2edb9bd6
serii burduja <seriiburduja@gmail.com>
2024-09-26 16:35:04 +0300

    feat: first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# logo
+# Compare A: Cristiano Ronaldo, a Footballer, from Portugal.
+# Logog
+# Against B: Vin Diesel, a Actor, from United States.
+# Who has more followers? Type 'A' or 'B': l
+# if you win, Last score will became A
+def higherLower():
+    pass
+
+higherLower()
